0618782b1b5dc95c52dfeb27fb0f38252103c2d1
jb-kim <jb.kim@tossinvest.com>
2023-09-24 22:06:28 +0900

    add kotest-spring-extention

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
     testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
 }
 
 tasks.withType<KotlinCompile> {
--- src/test/kotlin/hello/springtx/apply/TxBasicTest.kt
+++ src/test/kotlin/hello/springtx/apply/TxBasicTest.kt
@@ -1,8 +1,13 @@
 package hello.springtx.apply
 
 import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.shouldBe
 import mu.KotlinLogging
+import org.springframework.aop.support.AopUtils
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.transaction.support.TransactionSynchronizationManager
@@ -10,24 +15,37 @@ import org.springframework.transaction.support.TransactionSynchronizationManager
 val log = KotlinLogging.logger {}
 
 @SpringBootTest
-class TxBasicTest : ShouldSpec(
+class TxBasicTest(
+    private val basicService: BasicService
+): ShouldSpec(
     {
 
-    }
-) {
-    internal open class BasicService3 {
-        @Transactional
-        open fun tx() {
-            log.info("call tx")
-            val txActive: Boolean = TransactionSynchronizationManager.isActualTransactionActive()
-            log.info("tx active={}", txActive) // true
+        should("Proxy Check") {
+            log.info { "aop class=${basicService.javaClass}" }
+            AopUtils.isAopProxy(basicService) shouldBe true
         }
 
-        fun nonTx() {
-            log.info("call nonTx")
-            val txActive: Boolean = TransactionSynchronizationManager.isActualTransactionActive()
-            log.info("tx active={}", txActive) // false
-        }
+    }
+)
+
+@TestConfiguration
+class TxApplyBasicConfig {
+    @Bean
+    fun basicService() = BasicService()
+}
+
+@Service
+class BasicService {
+    @Transactional
+    fun tx() {
+        log.info("call tx")
+        val txActive: Boolean = TransactionSynchronizationManager.isActualTransactionActive()
+        log.info("tx active={}", txActive) // true
     }
 
+    fun nonTx() {
+        log.info("call nonTx")
+        val txActive: Boolean = TransactionSynchronizationManager.isActualTransactionActive()
+        log.info("tx active={}", txActive) // false
+    }
 }
